3a6cfb33eecc3cc4df2a945e7cc057c5505aa101
Vza <35027648+voza95@users.noreply.github.com>
2023-10-30 22:32:15 +0530

    Added swap log

--- day-1.py
+++ day-1.py
@@ -1,3 +1,12 @@
 name = input("What is you name?\n")
 print("Hello " + name + "!!\n")
 print("You have " + str(len(name)) + " characters in your name.\n")
+
+print("Now let's switch two vaiables.")
+a = input("a: ")
+b = input("b: ")
+
+print("Final output after swap:")
+a, b = b, a
+print("a: " + a)
+print("b: " + b)
