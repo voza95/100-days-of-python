6dcf78ff765641b35ce069bd88155ead903f2bcb
Vza <35027648+voza95@users.noreply.github.com>
2023-10-31 22:27:47 +0530

    number operations

---
+++ day-2.py
@@ -0,0 +1,20 @@
+first_large_number = 123_456
+second_large_number = 987_654.232
+result_str = str(first_large_number) + str(second_large_number)
+result = first_large_number + second_large_number
+print(result)
+print(result_str)
+print(type(result))
+
+print("##################################")
+
+user_number = input("Enter a two digit number: ")
+
+num_1 = int(user_number[0])
+num_2 = int(user_number[1])
+
+print("Sum of two digit number is: " + str(num_1 + num_2))
+
+
+
+print(2 ** 3)
