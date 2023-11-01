c25923ca9aea1d65e3c6360a5160b70fc2e5c118
Vza <35027648+voza95@users.noreply.github.com>
2023-11-01 22:40:50 +0530

    multiple conditional statment

--- day-3.py
+++ day-3.py
@@ -1,11 +1,13 @@
 print("Welcome to the rollercoaster!")
 height = int(input("Enter you height in cm: "))
-age = int(input("Enter your age:"))
+age = int(input("Enter your age: "))
 
 if height > 120:
     if age >= 18:
         print("You can ride the rollercoaster! and you're price is $12.")
-    else:
+    elif age < 18 and age > 12:
         print("You can ride the rollercoaster! and you're price is $7.")
+    else:
+        print("You can ride the rollercoaster! and you're price is $5.")
 else:
     print("Sorry, you can't go to rollercoaster yet!")
