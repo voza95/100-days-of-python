bab193151177d2be7e2bdd01d907d7a00ab8471e
Vza <35027648+voza95@users.noreply.github.com>
2023-11-01 22:38:14 +0530

    Nested condition statment

--- day-3.py
+++ day-3.py
@@ -1,7 +1,11 @@
 print("Welcome to the rollercoaster!")
-height = int(input("Enter you height in cm? "))
+height = int(input("Enter you height in cm: "))
+age = int(input("Enter your age:"))
 
 if height > 120:
-    print("You can ride the rollercoaster!")
+    if age >= 18:
+        print("You can ride the rollercoaster! and you're price is $12.")
+    else:
+        print("You can ride the rollercoaster! and you're price is $7.")
 else:
     print("Sorry, you can't go to rollercoaster yet!")
