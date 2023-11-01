1a09831a067c0a81ce4be9277a3f74376e34ecf4
Vza <35027648+voza95@users.noreply.github.com>
2023-11-01 22:32:35 +0530

    day 3 conditional statement

---
+++ day-3.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("Enter you height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you can't go to rollercoaster yet!")
