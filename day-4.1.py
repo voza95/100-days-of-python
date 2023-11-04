6a49ff15d57d5831bd37b2889fd1b1cc6c275dd6
Vza <35027648+voza95@users.noreply.github.com>
2023-11-04 17:24:10 +0530

    Random module

---
+++ day-4.1.py
@@ -0,0 +1,10 @@
+import random
+
+print("Heads or Tails")
+
+h_or_t = random.randint(0, 1)
+
+if h_or_t == 1:
+    print("You flip a Head")
+else:
+    print("You flip a tail")
---
+++ day-4.py
@@ -0,0 +1,8 @@
+import random
+
+random_interger = random.randint(1,10)
+print(random_interger)
+
+random_float = random.random()
+print(random_float)
+
